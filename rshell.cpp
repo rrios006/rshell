8798c42945bc3ab36d279cd52a27097a771923ac
Ricardo Rios <rrios006@hammer.cs.ucr.edu>
2017-02-16 22:06:24 -0800

    Add all files for hammer

---
+++ rshell.cpp
@@ -0,0 +1,10 @@
+
+
+void exit(); // used to end shell
+void parse(); // will parse the input
+void print(); // will print user input
+void excute(); // excute commands
+
+int main(int argc, char *argv[]){
+	
+		
