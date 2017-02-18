b4a044af455edb01a7965e3fb69b548a674089da
Ricardo Rios <rrios006@hammer.cs.ucr.edu>
2017-02-17 20:54:52 -0800

    updated rshell.cpp

--- rshell.cpp
+++ rshell.cpp
@@ -1,10 +1,61 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <cstring.h>
+#include <dirent.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <pwd.h>
+#include <errno.h>
+#include <grp.h>
+#include <time.h>
 
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <vector>
+#include <algorithm>
 
 void exit(); // used to end shell
 void parse(); // will parse the input
 void print(); // will print user input
-void excute(); // excute commands
+// excute commands
+void execute(string x)
+{
+	int status;
+	pid_t pid= fork();
+	if (pid < 0){
+		perror("Forking child process failed");
+		exit();
+	}
+	else if (pid == 0) {
+		if (execvp(*x, x) < 0) {
+			perrror("Execute failed");
+			exit();
+		}
+	}
+	else {
+		waitpit(pid,&status,0);
+	}
+}
+			
+			
+int main(int argc, char *argv[])
+{
+	for (i = 0; i < argc; i++)
+	{
+  		//will get every input
+		char[] input = argv[i];
 
-int main(int argc, char *argv[]){
-	
-		
+		//check for ;
+		if(strchr(input,';')-input!=NULL){
+			int in = strchr(input,';')-input;
+			input[in] = '\0';
+		}
+        	
+		//
+	}
+}
+        	
