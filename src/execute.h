3a6698f6ac25f16aae9b36849dc996c46b0ca89a
rrios006 <rrios006@ucr.edu>
2017-03-04 04:58:32 +0000

    Made execute.h

---
+++ src/execute.h
@@ -0,0 +1,25 @@
+#ifndef _EXECUTE_H_
+#define _EXECUTE_H_
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <pwd.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <limits.h>
+
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
